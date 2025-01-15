e8d7ba29ea947ac293c3912476b0891f0b6642d0
Eric Olerud <eric@olerud.com>
2025-01-14 22:18:41 -0500

    finish 1a

---
+++ aoc_1a/src/main.rs
@@ -0,0 +1,31 @@
+use anyhow::Result;
+use std::fs;
+
+fn main() -> Result<()> {
+    let contents = fs::read_to_string("./input")?;
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    for line in contents.lines() {
+        for (i, val) in line.split_whitespace().enumerate() {
+            let converted: i32 = val.parse::<i32>()?;
+            if i % 2 == 0 {
+                left.push(converted);
+            } else {
+                right.push(converted);
+            }
+        }
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut result = 0;
+    for i in 0..left.len() {
+        result += left[i].abs_diff(right[i]);
+    }
+
+    println!("{result}");
+    Ok(())
+}

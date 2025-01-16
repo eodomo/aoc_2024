441e5382d78a2f37cc78e82cd7fe5da6000f80b9
Eric Olerud <eric@olerud.com>
2025-01-16 17:08:24 -0500

    continue updating aoc_2a

--- aoc_2a/src/main.rs
+++ aoc_2a/src/main.rs
@@ -6,32 +6,39 @@ enum Direction {
     Increasing,
     Decreasing,
 }
-struct Level {
-    level: Vec<i32>,
+struct Report {
+    levels: Vec<i32>,
     direction: Direction,
 }
-impl Level {
+impl Report {
     fn new() -> Self {
-        Level {
-            level: vec![],
+        Report {
+            levels: vec![],
             direction: Direction::Increasing,
         }
     }
     fn set_direction(&mut self) {
-        if self.level.len() < 2 {
+        if self.levels.len() < 2 {
             panic!(
                 "Level does not contain two or more entries: {:?}",
-                self.level
+                self.levels
             );
         }
-        if self.level[1] > self.level[0] {
+        if self.levels[1] > self.levels[0] {
             self.direction = Direction::Increasing
         } else {
             self.direction = Direction::Decreasing
         }
     }
     /// set_direction must be run before this
-    fn compare_values(&self) -> bool {}
+    fn compare_values_increasing(&self) -> bool {
+        let mut iter = self.levels.iter().peekable();
+        for level in iter {
+            let next_level = iter.peek();
+            
+        }
+        true
+    }
 }
 
 fn main() -> Result<()> {

1c008438984b24feadf2ab98ee009b3c60ab944a
Eric Olerud <eric@olerud.com>
2025-01-19 18:43:54 -0500

    add more comparisons

--- aoc_2a/src/main.rs
+++ aoc_2a/src/main.rs
@@ -1,50 +1,95 @@
 use anyhow::Result;
-use core::panic;
 use std::fs;
 
+#[derive(PartialEq, Eq, Clone, Copy)]
 enum Direction {
     Increasing,
     Decreasing,
 }
 struct Report {
     levels: Vec<i32>,
-    direction: Direction,
+    direction: Option<Direction>,
     safe: bool,
 }
 impl Report {
     fn new() -> Self {
         Report {
             levels: vec![],
-            direction: Direction::Increasing,
+            direction: None,
             safe: true,
         }
     }
-    fn set_direction(&mut self) {
-        if self.levels.len() < 2 {
-            panic!(
-                "Level does not contain two or more entries: {:?}",
-                self.levels
-            );
-        }
-        if self.levels[1] > self.levels[0] {
-            self.direction = Direction::Increasing
-        } else {
-            self.direction = Direction::Decreasing
-        }
-    }
-    /// set_direction must be run before this
-    fn compare_values_increasing(&self) -> bool {
-        let mut iter = self.levels.iter().peekable();
-        for level in iter {
-            let next_level = iter.peek();
+    fn compare_all_values(&mut self) -> bool {
+        let window = self.levels.windows(2);
+        for slice in window {
+            match self.direction {
+                None => {
+                    let direction = compare_direction(&slice[0], &slice[1]);
+                    self.direction = Some(direction)
+                }
+                Some(val) => {
+                    if compare_direction(&slice[0], &slice[1]) != val {
+                        self.safe = false;
+                        return false;
+                    }
+                    if !compare_two_levels(val, &slice[0], &slice[1]) {
+                        self.safe = false;
+                        return false;
+                    }
+                }
+            }
         }
         true
     }
 }
 
+/// true means safe, false means unsafe
+fn compare_two_levels(direction: Direction, num0: &i32, num1: &i32) -> bool {
+    if direction == Direction::Increasing {
+        if num0 > num1 {
+            false
+        } else {
+            true
+        }
+    } else {
+        if num0 > num1 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn compare_direction(num0: &i32, num1: &i32) -> Direction {
+    if num1 > num0 {
+        Direction::Increasing
+    } else {
+        Direction::Decreasing
+    }
+}
+
 fn main() -> Result<()> {
     let contents = fs::read_to_string("./input")?;
-    let reports = contents.lines();
+    let lines = contents.lines();
+    let mut reports: Vec<Report> = Vec::new();
 
+    for line in lines {
+        let mut report = Report::new();
+        for (_i, level) in line.split_whitespace().enumerate() {
+            let converted: i32 = level.parse::<i32>()?;
+            report.levels.push(converted);
+        }
+        reports.push(report);
+    }
+
+    let mut safe_count = 0;
+    for report in reports.iter_mut() {
+        report.compare_all_values();
+        if report.safe == true {
+            safe_count += 1;
+        }
+    }
+
+    println!("{safe_count}");
     Ok(())
 }
